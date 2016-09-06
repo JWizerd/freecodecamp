46346026840b09a6adbe21c609c021559fab3abb
JWizerd <jwodke@outlook.com>
2016-09-06 09:33:34 -0600

    basic algorithm problems, finished problem 1

---
+++ frontend/freecodecamp-js-basic-algorithm-problems.js
@@ -0,0 +1,10 @@
+// 1. Reverse a String
+
+// split string into an array and save its state
+// for each character in the array pop the last character off and push it into an empty array
+// convert the array to a string and the reassign it to string.
+
+function reverseString(str) {
+  str.split('').reverse().join('');
+  return str;
+}
