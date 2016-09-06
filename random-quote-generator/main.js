c756a84c25a7b696a56b61a609ac8d5ac8eb7330
JWizerd <jwodke@outlook.com>
2016-09-06 11:16:08 -0600

    random quote generator finished

--- random-quote-generator/main.js
+++ random-quote-generator/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+  getRandomQuote();
+});
 
+function getRandomQuote(){
+  url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+
+  $("#quote-generator-link").on("click", function(){
+    $.getJSON(url, function(data){
+      $("#quote").text(data.quoteText);
+      $("#author").text(data.quoteAuthor);
+    }).fail(function(){
+      alert("failure");
+    });
+  });
+}
