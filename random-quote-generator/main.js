fe5bb5f52a9aafd6e35fc168b7e564591346f12b
JWizerd <jwodke@outlook.com>
2016-09-06 11:59:43 -0600

    added twitter tweet quote functionality

--- random-quote-generator/main.js
+++ random-quote-generator/main.js
@@ -7,10 +7,25 @@ function getRandomQuote(){
 
   $("#quote-generator-link").on("click", function(){
     $.getJSON(url, function(data){
+      $("#tweet").css("display", "block");
       $("#quote").text(data.quoteText);
       $("#author").text(data.quoteAuthor);
+      var textToTweet = data.quoteText;
+      sendATweet(textToTweet);
     }).fail(function(){
       alert("failure");
     });
+  });
+}
+
+function sendATweet(text){
+  $("#tweet").on("click", function(){
+    if (text.length > 140) {
+      alert("this quote is too long please generate another one");
+    }
+    else {
+      var tweetLink = 'http://twitter.com/home?status=' +encodeURIComponent(text);
+      window.open(tweetLink, '_blank');
+    }
   });
 }
